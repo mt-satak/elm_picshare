1f000022a2050cae0396130703f166ac40b15d0f
mt-satak <sakiware@gmail.com>
2021-03-11 21:36:35 +0900

    初期化用モデルとビューを作成してmain関数を軽くする

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -6,22 +6,29 @@ import Html.Attributes exposing (class, src)
 baseUrl : String
 baseUrl = "https://programming-elm.com/"
 
-viewDetailedPhoto : String -> String -> Html msg
-viewDetailedPhoto url caption = 
+initialModel : { url : String, caption : String }
+initialModel = 
+  { url = baseUrl ++ "1.jpg"
+  , caption = "Surfing"
+  }
+
+viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto model = 
   div [ class "detail-photo" ]
-      [ img [ src url ] []
+      [ img [ src model.url ] []
       , div [ class "photo-info" ]
-        [ h2 [ class "caption" ] [ text caption ] ]
+        [ h2 [ class "caption" ] [ text model.caption ] ]
       ]
 
-main : Html msg
-main = 
+view : { url : String, caption : String } -> Html msg
+view model = 
   div []
     [ div [ class "header" ]
       [ h1 [] [ text "Picshare" ] ]
     , div [ class "content-flow" ]
-      [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
-      , viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
-      , viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
-      ]
+      [ viewDetailedPhoto model ]
     ]
+
+main : Html msg
+main = 
+  view initialModel
